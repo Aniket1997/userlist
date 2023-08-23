68dc997038f6232f285e1b8237ce40363542bbe7
Aniket Paul <16riju@gmail.com>
2023-08-24 01:03:58 +0530

    Jquery

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', async function () {
-    const form = document.getElementById('dataForm');
-    const tableBody = document.querySelector('#dataTable tbody');
+$(document).ready(async function () {
+    const form = $('#dataForm');
+    const tableBody = $('#dataTable tbody');
 
     async function fetchData() {
         const response = await fetch('http://localhost:3000/data');
@@ -8,50 +8,43 @@ document.addEventListener('DOMContentLoaded', async function () {
         data.forEach(entry => {
             entry.dateOfBirth = new Date(entry.dateOfBirth);
         });
-    
         return data;
     }
-    
 
     async function renderData() {
         const data = await fetchData();
-    
-        // Sort the data based on age
         data.sort((a, b) => a.age - b.age);
-    
-        tableBody.innerHTML = '';
+        tableBody.empty();
         data.forEach(entry => {
             const dobFormatted = entry.dateOfBirth.toLocaleDateString('en-US', {
                 day: 'numeric',
                 month: 'short',
                 year: 'numeric'
             });
-    
-            const newRow = document.createElement('tr');
-            newRow.innerHTML = `
+
+            const newRow = $('<tr></tr>');
+            newRow.html(`
                 <td>${entry.name}</td>
                 <td>${dobFormatted}</td>
                 <td style="text-align: center">${entry.age}</td>
                 <td class="action-buttons">
                     <button class="edit-button" data-id="${entry.id}">Edit</button>
                     <button class="delete-button" data-id="${entry.id}">Delete</button>
-                </td>`;
-            tableBody.appendChild(newRow);
+                </td>`);
+            tableBody.append(newRow);
         });
     }
-    
-    
+
     async function updateData(id, name, dateOfBirth, age) {
         const response = await fetch(`http://localhost:3000/data/${id}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ name, dateOfBirth, age }) // Include the newAge value
+            body: JSON.stringify({ name, dateOfBirth, age })
         });
         return response.ok;
     }
-    
 
     async function deleteData(id) {
         const response = await fetch(`http://localhost:3000/data/${id}`, {
@@ -61,113 +54,107 @@ document.addEventListener('DOMContentLoaded', async function () {
     }
 
     async function handleEditClick(row) {
-        const id = row.querySelector('.edit-button').dataset.id;
-        const nameCell = row.querySelector('td:nth-child(1)');
-        const dobCell = row.querySelector('td:nth-child(2)');
-        const ageCell = row.querySelector('td:nth-child(3)');
-        const actionCell = row.querySelector('.action-buttons');
-    
-        const name = nameCell.textContent;
-        const dob = dobCell.textContent; // Get the original date of birth as text
-        const age = ageCell.textContent;
-    
-        nameCell.innerHTML = `<input type="text" class="edit-name" value="${name}" style="width: 180px;" />`;
-        dobCell.innerHTML = `<input type="date" class="edit-dob" value="${dob}" style="width: 98%;
+        const id = $(row).find('.edit-button').data('id');
+        const nameCell = $(row).find('td:nth-child(1)');
+        const dobCell = $(row).find('td:nth-child(2)');
+        const ageCell = $(row).find('td:nth-child(3)');
+        const actionCell = $(row).find('.action-buttons');
+
+        const name = nameCell.text();
+        const dob = dobCell.text();
+        const age = ageCell.text();
+
+        nameCell.html(`<input type="text" class="edit-name" value="${name}" style="width: 180px;" />`);
+        dobCell.html(`<input type="date" class="edit-dob" value="${dob}" style="width: 98%;
         border-radius: 6px;
         height: 41px;
-        border: 1px solid #c5c5c5;" />`; // Set the width here
-        ageCell.innerHTML = `<span class="edit-age">${age}</span>`; // Display the age as a span
-        row.querySelector('.edit-button').textContent = 'Save';
-    
-        actionCell.classList.add('expanded-cell'); // Increase cell width during editing
+        border: 1px solid #c5c5c5;" />`);
+        ageCell.html(`<span class="edit-age">${age}</span>`);
+        $(row).find('.edit-button').text('Save');
+
+        actionCell.addClass('expanded-cell');
     }
-    
+
     async function handleSaveClick(row) {
-        const id = row.querySelector('.edit-button').dataset.id;
-        const newName = row.querySelector('.edit-name').value;
-        const newDob = row.querySelector('.edit-dob').value;
-    
-        // Calculate new age based on the updated date of birth
+        const id = $(row).find('.edit-button').data('id');
+        const newName = $(row).find('.edit-name').val();
+        const newDob = $(row).find('.edit-dob').val();
+
         const dob = new Date(newDob);
         const today = new Date();
         const newAge = today.getFullYear() - dob.getFullYear() - (today.getMonth() < dob.getMonth() || (today.getMonth() === dob.getMonth() && today.getDate() < dob.getDate()) ? 1 : 0);
-    
-        const success = await updateData(id, newName, newDob, newAge); // Pass newAge to updateData
-    
-        if (success) {
-            renderData();
-        }
-    
-        row.querySelector('.delete-button').style.display = 'block';
-        const actionCell = row.querySelector('.action-buttons');
-        actionCell.classList.remove('expanded-cell');
-    }
-    
-    
 
+        const success = await updateData(id, newName, newDob, newAge);
+
+        if (success) {
+            await renderData();
+        }
+
+        $(row).find('.delete-button').css('display', 'block');
+        const actionCell = $(row).find('.action-buttons');
+        actionCell.removeClass('expanded-cell');
+    }
 
     async function handleDeleteClick(id) {
         const success = await deleteData(id);
 
         if (success) {
-            renderData();
+            await renderData();
         }
     }
 
-    form.addEventListener('submit', async function (e) {
+    form.on('submit', async function (e) {
         e.preventDefault();
-    
-        const nameInput = document.getElementById('name');
-        const dateOfBirthInput = document.getElementById('dateOfBirth');
-        const ageInput = document.getElementById('age');
-    
-        const name = nameInput.value;
-        const dateOfBirth = dateOfBirthInput.value; // Get the value of the dateOfBirth input
-        const age = ageInput.value;
-    
+
+        const nameInput = $('#name');
+        const dateOfBirthInput = $('#dateOfBirth');
+        const ageInput = $('#age');
+
+        const name = nameInput.val();
+        const dateOfBirth = dateOfBirthInput.val();
+        const age = ageInput.val();
+
         if (name && age) {
             const response = await fetch('http://localhost:3000/data', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ name, dateOfBirth, age }) // Use the dateOfBirth value
+                body: JSON.stringify({ name, dateOfBirth, age })
             });
-    
+
             if (response.ok) {
-                await renderData(); // Refresh the table after adding new data
-    
-                nameInput.value = '';
-                ageInput.value = '';
+                await renderData();
+
+                nameInput.val('');
+                ageInput.val('');
             }
         }
     });
-    
 
-    tableBody.addEventListener('click', async function (e) {
-        if (e.target.classList.contains('edit-button')) {
-            const row = e.target.closest('tr');
-            if (e.target.textContent === 'Edit') {
+    tableBody.on('click', async function (e) {
+        if ($(e.target).hasClass('edit-button')) {
+            const row = $(e.target).closest('tr');
+            if ($(e.target).text() === 'Edit') {
                 handleEditClick(row);
-            } else if (e.target.textContent === 'Save') {
+            } else if ($(e.target).text() === 'Save') {
                 handleSaveClick(row);
             }
-        } else if (e.target.classList.contains('delete-button')) {
-            const id = e.target.dataset.id;
+        } else if ($(e.target).hasClass('delete-button')) {
+            const id = $(e.target).data('id');
             handleDeleteClick(id);
         }
     });
 
-    const dateOfBirthInput = document.getElementById('dateOfBirth');
-    const ageInput = document.getElementById('age');
+    const dateOfBirthInput = $('#dateOfBirth');
+    const ageInput = $('#age');
 
-    dateOfBirthInput.addEventListener('change', () => {
-        const dob = new Date(dateOfBirthInput.value);
+    dateOfBirthInput.on('change', () => {
+        const dob = new Date(dateOfBirthInput.val());
         const today = new Date();
         const age = today.getFullYear() - dob.getFullYear() - (today.getMonth() < dob.getMonth() || (today.getMonth() === dob.getMonth() && today.getDate() < dob.getDate()) ? 1 : 0);
-        ageInput.value = age;
+        ageInput.val(age);
     });
-
 
     await renderData();
 });
